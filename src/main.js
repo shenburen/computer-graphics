af2beaf3ed16fd3f670881371c8ebef313424952
shenburen <593446878@qq.com>
2024-01-08 11:52:50 +0800

    提交。

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
-alert(1);
+const pages = ["test1", "test2"];
+
+let btns = document.querySelectorAll(".btn");
+let iframe = document.querySelector("iframe");
+
+for (let i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", () => {
+    iframe.src = "./src/pages/" + pages[i] + ".html";
+  });
+}
